98ea0be4cc895c56a5b643172ff8225050226ffa
Melya <melenko55@gmail.com>
2022-08-10 11:43:32 +0300

    Change test for greeting and add new one for App

---
+++ client/src/App.test.js
@@ -0,0 +1,18 @@
+import App from "./App"
+import {fireEvent, render} from "@testing-library/react"
+import store from "./store/store"
+import { Provider } from "react-redux"
+
+it("App renders", () => {
+    const { asFragment } = render(<Provider store={store}><App /></Provider>)
+    expect(asFragment()).toMatchSnapshot()
+})
+
+describe('Greeting test', () => {
+    it('Button is disabled if race have started', () => {
+        const greeting = render(<Provider store={store}><App /></Provider>)
+        const btn = greeting.getByTestId("greeting-btn")
+        fireEvent(btn, new MouseEvent('click'))
+        expect(btn.disabled).toBe(true)
+    })
+})
